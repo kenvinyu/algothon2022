7945af0de9416c44791eaa8a69523ff58effbf0d
kenvinyu <kenvinyu2000@gmail.com>
2022-07-13 08:56:49 +0000

    shit algo

--- teamName.py
+++ teamName.py
@@ -1,13 +1,24 @@
 import numpy as np
 
-nInst=100
-currentPos = np.zeros(nInst)
+import experimental_strategies as es
 
 def getMyPosition (prcSoFar):
-    global currentPos
+    indxs = [2, 6, 60, 76, 91, 92, 95]
 
-    # Build your function body here
+    if prcSoFar.shape[1] <= 1: 
+        return np.zeros(prcSoFar.shape[0])
+    p = prcSoFar[indxs, :]
+    p_log = np.log(p)
+    p_log_diff = np.diff(p_log, axis=1)
 
-    return currentPos
+    p_log_diff_last = p_log_diff[-1]
+    m = np.mean(p_log_diff_last)
 
-    
+    buys = np.zeros(prcSoFar.shape[0], dtype=bool)
+    np.put(buys, indxs, p_log_diff_last < m - 0.0005)
+
+    sells = np.zeros(prcSoFar.shape[0], dtype=bool)
+    np.put(sells, indxs, p_log_diff_last > m + 0.0005)
+
+    pos_limits = np.ceil(10000 / prcSoFar[:,-1])
+    return pos_limits * buys - pos_limits * sells
